e6892f020917e313ae2f2324980bec9283d74ec9
chiragsawantt <101980473+chiragsawantt@users.noreply.github.com>
2022-11-21 00:16:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "asddsadsd")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5916446627:AAFT8j7sc30cp8BDFYj62z8d07KsnTH1jk4")
 # BOT_TOKEN = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"  #  @aryan_vbot
-APP_ID = int(os.environ.get("API_ID", "sdasdsd"))
-API_HASH = os.environ.get("API_HASH", "dsfdfsf")
+APP_ID = int(os.environ.get("API_ID", "16749444"))
+API_HASH = os.environ.get("API_HASH", "17a1ff9c04503e79dc546aab2de32c8a")
 AUTH_GROUP = ""
 
 Creds_path = "creds"
 DOWNLOAD_LOCATION = "/"
 
-adminList = [985378987, 1212106123]
+adminList = [985378987, 1212106123, 895281037]
 
 
 EDIT_TIME = 5
